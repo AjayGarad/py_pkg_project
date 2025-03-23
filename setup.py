ee375378ebcb220b8e51257bfd18d1ff74cad796
ajayg <ajayg@dosepack.com>
2025-03-23 18:24:24 +0530

    udated gitignore and related data

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pkg_test_random",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[],
     author="Ajay Garad",
